b8847d053ad77be3108cb21053a905766281c8da
Bospur <Bospobob@yandex.ru>
2022-05-25 19:12:04 +0500

    модалка

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,38 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
+import PostFilter from './componets/PostFilter';
 import PostForm from './componets/PostForm';
 import PostList from './componets/PostList';
+import MyButton from './componets/UI/button/MyButton';
+import MyModal from './componets/UI/MyModal/MyModal';
 import './styles/App.css'
 
 
 const App = () => {
     const [posts, setPosts] = useState([
-        { id: 1, title:"JavaScript", body: "Description"},
-        { id: 2, title:"JavaScript", body: "Description"},
-        { id: 3, title:"JavaScript", body: "Description"}
+        { id: 1, title:"JavaScript", body: "мультипарадигменный язык программирования"},
+        { id: 2, title:"Python", body: "высокоуровневый язык программирования общего назначения с динамической строгой типизацией и автоматическим управлением памятью"},
+        { id: 3, title:"Java", body: "строго типизированный объектно-ориентированный язык программирования общего назначения, разработанный компанией Sun Microsystems."}
     ]);
-    
+    const [filter, setFilter] = useState({
+        sort: '',
+        query: ''
+    });
+    const [modal, setModal] = useState(false);
+
+    const sortedPosts = useMemo(() => {
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+        }
+        return posts
+    }, [filter.sort, posts])
+
+    const sortedAndSorchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+    }, [filter.query, sortedPosts])
+
     const createPost = (newPost) => {
-        setPosts([...posts, newPost])
+        setPosts([...posts, newPost]);
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -21,13 +41,24 @@ const App = () => {
     }
 
     return (
-       <div class="App">           
-            <PostForm create={createPost}/>
-            {
-                posts.length
-                ? <PostList posts={posts} title="Посты про JS" onRemove={removePost}/>
-                : <h1 style={{textAlign: 'center'}}>Постов не обнаруженно...</h1>
-            }           
+       <div class="App">
+           <MyButton style={{marginTop: '15px'}} onClick={() => setModal(true)}> Создать пост </MyButton>
+            <MyModal
+                visible={modal}
+                setVisible={setModal}
+            >
+                <PostForm create={createPost}/>    
+            </MyModal>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter 
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <PostList 
+                posts={sortedAndSorchedPosts}
+                title="Посты про языки программирования" 
+                onRemove={removePost}
+            />          
        </div>
     );
 }
